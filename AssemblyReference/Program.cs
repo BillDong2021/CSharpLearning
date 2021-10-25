655249700e9595aef756ed787a4aea354842f667
billdong2021 <billdong2018@gmail.com>
2021-10-25 21:39:46 +0800

    An example to learn how to use assembly reference

---
+++ AssemblyReference/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tools;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result1 = Calculator.Mul(3, 4);
+            Console.WriteLine(result1);
+            double result2 = Calculator.Div(3, 0);
+            Console.WriteLine(result2);
+        }
+    }
+}
