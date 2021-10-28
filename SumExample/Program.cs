33bdc18cccf523914d8e0f5997009a61451468a5
billdong2021 <billdong2018@gmail.com>
2021-10-28 10:03:58 +0800

    An example to create an instance method in a class and invoke it

---
+++ SumExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum mySum = new Sum();
+            int x = mySum.GetSum(100);
+            Console.WriteLine(x);
+        }
+    }
+
+    class Sum
+    {
+        public int GetSum(int x)
+        {
+            int result = 0;
+            for (int i = 0; i <= x; i++)
+            {
+                result = result + i;
+            }
+            return result;
+        }
+    }
+}
