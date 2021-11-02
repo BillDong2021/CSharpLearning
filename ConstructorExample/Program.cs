4b0eaaa5770eea12b7b077a385e26e26d9405733
billdong2021 <billdong2018@gmail.com>
2021-11-02 16:56:45 +0800

    An example to show how to create a constructor for a class

---
+++ ConstructorExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStu = new Student(2,"Tom");
+            Console.WriteLine(myStu.Name);
+            Console.WriteLine(myStu.ID);
+            Console.WriteLine("-------------------------");
+            Student myStu2 = new Student();
+            Console.WriteLine(myStu2.Name);
+            Console.WriteLine(myStu2.ID);
+        }
+    }
+    class Student
+    {
+        public Student(int initID,string initName)
+        {
+            this.ID = initID;
+            this.Name = initName;
+        }
+        public Student()
+        {
+            this.ID = 1;
+            this.Name = "No name";
+        }
+        public int ID;
+        public string Name;
+    }
+}
