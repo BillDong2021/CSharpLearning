5e583c9eacf5322bb1ddd1f9fb0ddf1395af0410
billdong2021 <billdong2018@gmail.com>
2021-11-03 21:29:10 +0800

    A example using -> operator to create and use pointer for a struct

---
+++ PointerExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                Student myStu = new Student();
+                myStu.ID = 1;
+                myStu.Score = 99;
+                Console.WriteLine(myStu.Score);
+                Student* pStu = &myStu;
+                pStu->Score = 100;
+                Console.WriteLine(myStu.Score);
+            }
+        }
+    }
+    struct Student
+    {
+        public int ID;
+        public double Score;
+    }
+}
