ba7e8a6c0b422f0c5bad2b207b20a2c5811a1901
billdong2021 <billdong2018@gmail.com>
2021-11-03 15:59:57 +0800

    An example to show how to use "new" and "var" to create an anonymous class and invoke its instance

---
+++ AnomynousClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnomynousClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Person = new { Name = "Tom", Age = 20 };
+            Console.WriteLine(Person.Age);
+            Console.WriteLine(Person.Name);
+            Console.WriteLine(Person.GetType().Name);
+        }
+    }
+}
