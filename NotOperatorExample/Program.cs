4b8a98dd9dc340ccf093533c4bb6eb647cd39a38
billdong2021 <billdong2018@gmail.com>
2021-11-03 22:04:42 +0800

    A example using NOT operator to prevent when creating a instance, the initial parameter in the constructor is forgotten

---
+++ NotOperatorExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotOperatorExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student("TOM");
+            Console.WriteLine(stu.Name);
+        }
+    }
+    class Student
+    {
+        public Student(string initName)
+        {
+            if(!string.IsNullOrEmpty(initName))
+            {
+                this.Name = initName;
+            }
+            else
+            {
+                throw new ArgumentException("initName cannot be null or empty");
+            }
+        }
+        public string Name;
+    }
+
+}
