921822bc4646c5c79663e84b7ce226a0d3b2705f
billdong2021 <billdong2018@gmail.com>
2021-11-07 20:30:36 +0800

    An example showing try+catch statement combined with"continue" statement to re-execute the loop when an error occurs in the loop

---
+++ DoWhileExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = -1; 
+            int sum = 100;
+            do
+            {
+                Console.WriteLine("Please input first number:");
+                string str1 = Console.ReadLine();
+                int x = 0;
+                try
+                {
+                    x = int.Parse(str1);
+                }
+                catch
+                {
+                    Console.WriteLine("First number has problem! Restart.");
+                    continue;//放弃继续执行，直接开始一次新的循环
+                }
+                Console.WriteLine("Please input second number:");
+                string str2 = Console.ReadLine();
+                int y = 0;
+                try 
+                { 
+                    y = int.Parse(str2); 
+                }
+                catch
+                {
+                    Console.WriteLine("Second number has problem! Restart.");
+                    continue;
+                }
+                sum = x + y;
+                score++;
+            } while (sum==100);
+            Console.WriteLine("Your score is {0}.",score);
+            Console.WriteLine("Game over");
+        }
+    }
+}
