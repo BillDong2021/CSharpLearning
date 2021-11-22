7fe530fc5f72427310d5f5698b7847cc8791536c
billdong2021 <billdong2018@gmail.com>
2021-11-22 20:15:12 +0800

    Event example

---
+++ EventExamples/EventExample1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace EventExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer();
+            myTimer.Interval = 1000;
+            myTimer.Elapsed += MyHandler.MyTimer_Elapsed;
+            myTimer.Start();
+            Console.ReadLine();
+        } 
+    }
+    class MyHandler
+    {
+        public static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("jump in {0}!", DateTime.Now.ToString()); 
+        }
+    }
+}
---
+++ EventExamples/EventExample2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyForm myForm = new MyForm();
+            myForm.Click += myForm.FormClicked;
+            myForm.ShowDialog();
+        }
+    }
+    class MyForm : Form
+    {
+        internal void FormClicked(object sender, EventArgs e)
+        {
+            this.Text = DateTime.Now.ToString();
+        }
+    }
+}
