c0bfe30dbc0b9b13b67442f96798dcb20b53c33e
billdong2021 <billdong2018@gmail.com>
2021-11-23 10:11:11 +0800

    An example showing how to override a base-class method in sub class

---
+++ OverrideExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vehicle myCar1 = new vehicle();
+            car myCar2 = new car();
+            myCar1.Run();
+            myCar2.Run();
+        }
+    }
+    class vehicle
+    {
+        public virtual void Run()
+        {
+            Console.WriteLine("I'm running!");
+        }
+    }
+    class car:vehicle
+    {
+        public override void Run()
+        {
+            Console.WriteLine("I'm running too!");
+        }
+    }
+}
